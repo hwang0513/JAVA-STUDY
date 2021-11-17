f1fc837a8278aeade794f1b9135a0e9789d13d07
hsj <tjdwlsvkdnj2@naver.com>
2021-11-17 15:15:54 +0900

    11월17일오후

---
+++ HelloWorld/src/hello/App.java
@@ -0,0 +1,20 @@
+package hello; // 패키지 헬로우
+
+public class App { // 클래스 App 만들기
+
+	public static void main(String[] args) {
+		// 메인 메소드에서 프로그램 시작
+		// 주석 단축키 ctrl+/
+		System.out.println("헬로우");
+		/*실행 방법 1. 이클립스 상단 run app, 2.오른쪽 마우스 run as>자바애플리케이션 
+		3. 왼쪽편 클래스파일을 선택하고run as>자바애플리케이션, 4. ctrl+f11 전부 실행되는건 main이 실행됨*/
+		//폴더설명
+		//Helloworld(프로젝트)
+		//이클립스 작업환경을 저장하기위함(.metadat)
+		//src폴더 안에 hello > 패키지(클래스(여기선App.java)를 모아둔 폴더)
+		//bin폴더 -> 컴퓨터가 이 폴더를 보고 실행함(컴퓨터가 보고 알수있는 파일), 마지막으로 컴파일된파일
+		
+		//새프로젝트 생성하고 안에 클래스 생성할때 메인메서드 체크하기
+	}
+
+}
