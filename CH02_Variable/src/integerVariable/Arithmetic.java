af879ac8bf4736706dd5a3e03af661250f8c073a
hsj <tjdwlsvkdnj2@naver.com>
2021-11-18 10:37:59 +0900

    11월 18일 오전 1

---
+++ CH02_Variable/src/integerVariable/Arithmetic.java
@@ -0,0 +1,17 @@
+package integerVariable;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(7 + 3);
+		System.out.println(9 / 3);
+		System.out.println(10 / 3); // 정수/정수 = 정수 
+		System.out.println(10.0 / 3.0);
+		System.out.println(9 / 4.0); // 정수(int4바이트)/실수(double8바이트) 이때 큰 값타입으로(실수로 나오게 됨)
+		System.out.println(3 * 4);
+		System.out.println(3 * 4.0); // 정수(int4바이트)/실수(double8바이트) 이때 큰 값타입으로(실수로 나오게 됨) 
+		System.out.println(3 - 4);
+	}
+
+}
